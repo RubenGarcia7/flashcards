c9a5b031a5e4e658b159862868e41da93b115eff
Ruben <hello@rubengarciabri.me>
2021-03-14 23:20:29 +0000

    added cards to flip automatically after each submission and changed styles

--- components/QuizCard.jsx
+++ components/QuizCard.jsx
@@ -13,6 +13,16 @@ const QuizCard = ({card, cardIndex, onAnswerRight, onAnswerWrong, deck}) => {
     setIsFlipped(!isFlipped)
   }
 
+  const handleOnAnswerRight = () => {
+    setIsFlipped(false)
+    onAnswerRight()
+  }
+
+  const handleOnAnswerWrong = () => {
+    setIsFlipped(false)
+    onAnswerWrong()
+  }
+
   return (
     <View style={styles.container}>
       {isFlipped === false ?
@@ -20,7 +30,8 @@ const QuizCard = ({card, cardIndex, onAnswerRight, onAnswerWrong, deck}) => {
           <Text style={styles.cardText}>{card.title}</Text>
         </View>  
         :
-        <View style={styles.cardContainer}>
+        <View style={styles.cardContainerAnswer}>
+           <Text style={styles.cardAnswer}>Answer: </Text>
           <Text style={styles.cardText}>{card.answer}</Text>
         </View>  
       } 
@@ -29,10 +40,10 @@ const QuizCard = ({card, cardIndex, onAnswerRight, onAnswerWrong, deck}) => {
         <Ionicons name="reload" size={20} color="white" style={styles.btnShowText}/>
       </TouchableOpacity>
       <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.btnWrong} onPress={() => onAnswerWrong()}>
+        <TouchableOpacity style={styles.btnWrong} onPress={() => handleOnAnswerRight()}>
           <Ionicons name="thumbs-down" size={27} color="white" style={styles.btnWrongText}/>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btnRight} onPress={() => onAnswerRight()}>
+        <TouchableOpacity style={styles.btnRight} onPress={() => handleOnAnswerRight()}>
           <Ionicons name="thumbs-up" size={27} color="white" style={styles.btnRightText}/>
         </TouchableOpacity>
       </View>
@@ -60,11 +71,27 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     padding: 20
   },
+  cardContainerAnswer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    padding: 20
+  },
   cardText: {
     color: '#fff',
     textAlign: 'center',
     fontSize: 18,
     fontWeight: '800'
+  },
+  cardAnswer: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+    marginBottom: 10
   },
   btnContainer: {
     flex: 1,
